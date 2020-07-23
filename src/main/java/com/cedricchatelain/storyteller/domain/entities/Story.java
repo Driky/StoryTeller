66382ef077bc004e04eac8713588c132c2d0d7d3
Driky <chatelain.cedric.pro@gmail.com>
2020-07-22 23:58:37 -0400

    Test and pass test for check on Paragraphs

--- src/main/java/com/cedricchatelain/storyteller/domain/entities/Story.java
+++ src/main/java/com/cedricchatelain/storyteller/domain/entities/Story.java
@@ -1,12 +1,33 @@
 package com.cedricchatelain.storyteller.domain.entities;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NonNull;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+
 @Builder
+@Getter
 public class Story {
     @NonNull
     private String id;
     @NonNull
     private Boolean isPublished;
+    @NonNull
+    private ArrayList<Paragraph> paragraphs;
+
+    private Story(@NonNull String id, @NonNull Boolean isPublished, @NonNull ArrayList <Paragraph> paragraphs) {
+        if(paragraphs.size() < 1) {
+            throw new InvalidParameterException("A Story require a least one Paragraph");
+        }
+
+        if(paragraphs.get(0).getType() != ParagraphType.TITLE) {
+            throw new InvalidParameterException("A Story require it's first Paragraph to be of type TITLE");
+        }
+
+        this.id = id;
+        this.isPublished = isPublished;
+        this.paragraphs = paragraphs;
+    }
 }
--- src/test/java/com/cedricchatelain/storyteller/domain/entities/StoryUTest.java
+++ src/test/java/com/cedricchatelain/storyteller/domain/entities/StoryUTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
@@ -37,11 +39,49 @@ public class StoryUTest {
         }
 
         @Test
+        void fail_when_Paragraphs_is_null() {
+            String id = "some_id";
+            Boolean isPublished = false;
+
+            Throwable throwable = catchThrowable(() -> Story.builder().isPublished(isPublished).id(id).build());
+
+            assertThat(throwable).isInstanceOf(java.lang.NullPointerException.class)
+                    .hasMessage("paragraphs is marked non-null but is null");
+        }
+
+        @Test
+        void fail_when_Paragraphs_is_empty() {
+            String id = "some_ID";
+            Boolean isPublished = false;
+            ArrayList <Paragraph> paragraphs = new ArrayList<Paragraph>();
+
+            Throwable throwable = catchThrowable(() -> Story.builder().isPublished(isPublished).id(id).paragraphs(paragraphs).build());
+
+            assertThat(throwable).isInstanceOf(java.security.InvalidParameterException.class)
+                    .hasMessage("A Story require a least one Paragraph");
+        }
+
+        @Test
+        void fail_when_first_paragraph_isnt_TITLE() {
+            String id = "some_ID";
+            Boolean isPublished = false;
+            ArrayList <Paragraph> paragraphs = new ArrayList<Paragraph>();
+            paragraphs.add(Paragraph.builder().name("name").type(ParagraphType.TEXT).build());
+
+            Throwable throwable = catchThrowable(() -> Story.builder().isPublished(isPublished).id(id).paragraphs(paragraphs).build());
+
+            assertThat(throwable).isInstanceOf(java.security.InvalidParameterException.class)
+                    .hasMessage("A Story require it's first Paragraph to be of type TITLE");
+        }
+
+        @Test
         void success_when_given_required_args() {
             Boolean isPublished = false;
             String id = "some_id";
+            ArrayList <Paragraph> paragraphs = new ArrayList<Paragraph>();
+            paragraphs.add(Paragraph.builder().name("name").type(ParagraphType.TITLE).build());
 
-            Story story = Story.builder().id(id).isPublished(isPublished).build();
+            Story story = Story.builder().isPublished(isPublished).id(id).paragraphs(paragraphs).build();
 
             assertThat(story).isInstanceOf(Story.class);
         }

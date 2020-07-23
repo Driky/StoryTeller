b512d24eeaa424d7fc18f46b102b964f2cacaae3
Driky <chatelain.cedric.pro@gmail.com>
2020-07-22 23:58:13 -0400

    Add necessary Getters

--- src/main/java/com/cedricchatelain/storyteller/domain/entities/Paragraph.java
+++ src/main/java/com/cedricchatelain/storyteller/domain/entities/Paragraph.java
@@ -1,9 +1,11 @@
 package com.cedricchatelain.storyteller.domain.entities;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NonNull;
 
 @Builder
+@Getter
 public class Paragraph {
     @NonNull
     private String name;

15addddeaab7f95ef8f2457d56158d38a412054d
Driky <chatelain.cedric.pro@gmail.com>
2020-07-22 22:34:07 -0400

    Starting to code with TDD. Reworking dirty code. Story first test

--- src/main/java/com/cedricchatelain/storyteller/domain/entities/Story.java
+++ src/main/java/com/cedricchatelain/storyteller/domain/entities/Story.java
@@ -1,6 +1,11 @@
 package com.cedricchatelain.storyteller.domain.entities;
 
-public class Story {
-    private Paragraph[] paragraphs;
+import lombok.Builder;
+import lombok.NonNull;
 
+@Builder
+public class Story {
+    private String id;
+    @NonNull
+    private Boolean isPublished;
 }
